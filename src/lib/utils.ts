897f8e9ddc3a84e3f56dcd3a8bb58d60a6fef549
Manugali <manugali@example.com>
2025-10-23 18:32:02 -0500

    Make short codes even shorter - 4 characters instead of 6

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,7 +19,7 @@ export function generateShortCode(): string {
   // Exclude similar-looking characters (0, O, I, l, 1)
   const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789'
   let result = ''
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 4; i++) {
     result += chars.charAt(Math.floor(Math.random() * chars.length))
   }
   return result
